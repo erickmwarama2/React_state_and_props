6e8ef1db327a1e44691d229b394f37162ec808db
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2022-10-27 10:36:14 +0300

    React state and props tutorial

---
+++ src/UpdateState.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+class UpdateState extends React.Component {
+
+    state = {
+        heading: "React Awesomesauce (Busy)",
+        content: "Loading..."
+    };
+
+    constructor() {
+        super();
+
+        setTimeout(() => {
+            this.setState({
+                heading: "React Awesomesauce",
+                content: "Done!"
+            });
+        }, 3000);
+    }
+
+    render() {
+
+        const {heading, content} = this.state;
+
+        return (
+            <main>
+                <h1>{heading}</h1>
+                <p>{content}</p>
+            </main>
+        );
+    }
+}
+
+export default UpdateState;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,63 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import MyComponent from "./MyComponent";
+import UpdateState from "./UpdateState";
+import MergeState from './MergeState';
+import DefaultProperties from './DefaultProperties';
+import MyButton from './MyButton';
+import MyList from './MyList';
+import MyButtonFunctional from './MyButtonFunctional';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const appState = {
+    text: "My Button",
+    disabled: true,
+    items: ["First", "Second", "Third"]
+};
+
+function render2(props) {
+    root.render(
+        <main>
+            <MyButton text={props.text} disabled={props.disabled}/>
+            <MyList items={props.items} />
+        </main>
+    )
+}
+
+function render({first, second}) {
+    root.render(
+        <main>
+            <MyButtonFunctional text={first.text} disabled={first.disabled} />
+            <MyButtonFunctional text={second.text} disabled={second.disabled} />
+        </main>
+    );
+}
+
+// render2(appState);
+render({
+    first: {
+        text: "My First Functional Button",
+        disabled: false
+    },
+    second: {
+        text: "Second Functional Button",
+        disabled: true
+    }
+});
+
+setTimeout(() => {
+    appState.disabled = false;
+    appState.items.push("Fourth");
+
+    // render2(appState);
+}, 3000);
+
+// root.render(<MyComponent/>);
+// root.render(<UpdateState/>);
+// root.render(<MergeState/>);
+// root.render(<DefaultProperties/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
